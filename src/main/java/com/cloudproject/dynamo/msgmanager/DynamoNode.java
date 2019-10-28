73290ca2cdd1a73a5e84042724d6d32dc92062d3
Shubham <sbhtwr170@gmail.com>
2019-10-29 03:23:08 +0530

    Added gossip protocol, testing left

--- src/main/java/com/cloudproject/dynamo/msgmanager/DynamoNode.java
+++ src/main/java/com/cloudproject/dynamo/msgmanager/DynamoNode.java
@@ -4,15 +4,17 @@ import com.cloudproject.dynamo.models.Node;
 
 import java.io.Serializable;
 
-public class DynamoNode implements Serializable, Node {
+public class DynamoNode implements Serializable, Cloneable {
     public String name;
     private String address;
     private int heartbeat;
+    private transient TimeoutTimer timeoutTimer;
 
-    public DynamoNode(String name, String address, int heartbeat) {
+    public DynamoNode(String name, String address, DynamoServer server, int heartbeat, int ttl) {
         this.name = name;
         this.address = address;
         this.heartbeat = heartbeat;
+        this.timeoutTimer = new TimeoutTimer(ttl, server, this);
     }
 
     public String getAddress() {
@@ -27,8 +29,43 @@ public class DynamoNode implements Serializable, Node {
         this.heartbeat = heartbeat;
     }
 
+    public void startTimer() {
+        this.timeoutTimer.start();
+    }
+
+    public void resetTimer() {
+        this.timeoutTimer.reset();
+    }
+
     @Override
     public String toString() {
         return "DynamoNode <address=" + address + ", heartbeat=" + heartbeat + ">";
+    }
+
+//    @Override
+//    protected Object clone() {
+//        return new DynamoNode(name, address, heartbeat, );
+//    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        DynamoNode other = (DynamoNode) obj;
+        if (address == null) {
+            if (other.address != null) {
+                return false;
+            }
+        } else if (!address.equals(other.address)) {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/com/cloudproject/dynamo/msgmanager/DynamoServer.java
+++ src/main/java/com/cloudproject/dynamo/msgmanager/DynamoServer.java
@@ -1,38 +1,61 @@
 package com.cloudproject.dynamo.msgmanager;
 
+import javax.management.NotificationListener;
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class DynamoServer {
+import javax.management.Notification;
 
-    private ArrayList<DynamoNode> nodeList;
-    private ArrayList<DynamoNode> deadList;
+public class DynamoServer implements NotificationListener {
+
+    private final ArrayList<DynamoNode> nodeList;
+    private final ArrayList<DynamoNode> deadList;
 
     private DatagramSocket server;
     private String address;
     private DynamoNode node;
+    private Random random;
+    private int gossipInt;
+    private int ttl;
 
-    public DynamoServer(String name, String address, ArrayList<String> addr_list) throws SocketException {
+    public DynamoServer(String name, String address, ArrayList<String> addr_list, int gossipInt, int ttl) throws SocketException {
         this.address = address;
         this.nodeList = new ArrayList<>();
+        this.deadList = new ArrayList<>();
+        this.gossipInt = gossipInt;
+        this.ttl = ttl;
         for (String addr : addr_list) {
-            this.nodeList.add(new DynamoNode(null, addr, 0));
+            this.nodeList.add(new DynamoNode(null, addr, this,0, ttl));
         }
-        this.node = new DynamoNode(name, address, 0);
+        this.node = new DynamoNode(name, address, this,0, ttl);
         int port = Integer.parseInt(address.split(":")[1]);
 
+        /* init Random */
+        this.random = new Random();
         /* Listen at port number port */
         this.server = new DatagramSocket(port);
         System.out.println("[Dynamo Server] Listening at port: " + port);
+    }
+
+    @Override
+    public void handleNotification(Notification notification, Object o) {
+        DynamoNode deadNode = (DynamoNode)notification.getUserData();
+        System.out.println(">> Server leave: " + deadNode.name + "has left the network");
+        synchronized (DynamoServer.this.nodeList) {
+            DynamoServer.this.nodeList.remove(deadNode);
+        }
+
+        synchronized (DynamoServer.this.deadList) {
+            DynamoServer.this.deadList.add(deadNode);
+        }
     }
 
     private class Receiver implements Runnable {
@@ -61,7 +84,7 @@ public class DynamoServer {
                                 System.out.println("[Dynamo Server] PING recieved from " + msg.srcNode.name);
                                 break;
                             case NODE_LIST:
-                                /* TODO: Implement NODE LIST RECV */
+                                DynamoServer.this.mergeMembershipLists(msg.srcNode, msg.payload);
                                 break;
                             default:
                                 System.out.println("Unrecognized packet type: " + msg.type.name());
@@ -129,10 +152,119 @@ public class DynamoServer {
         }
     }
 
+    private class Gossiper implements Runnable {
+        private AtomicBoolean keepRunning;
+
+        Gossiper() {
+            keepRunning = new AtomicBoolean(true);
+        }
+
+        public void run() {
+            while (this.keepRunning.get()) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(DynamoServer.this.gossipInt);
+                    DynamoServer.this.sendMembershipList();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    this.keepRunning.set(false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            this.keepRunning = null;
+        }
+    }
+
+    private DynamoNode getRandomNode() {
+        DynamoNode node = null;
+
+        if (this.nodeList.size() > 0) {
+            int rand = random.nextInt(this.nodeList.size());
+            node = this.nodeList.get(rand);
+        }
+
+        return node;
+    }
+
+    private void sendMembershipList() throws IOException {
+        this.node.setHeartbeat(this.node.getHeartbeat() + 1);
+        //ArrayList<DynamoNode> sendList = cloneArrayList(this.nodeList);
+        synchronized (this.nodeList) {
+            DynamoNode dstNode = this.getRandomNode();
+            if (node != null) {
+                DynamoMessage listMsg =
+                        new DynamoMessage(this.node, MessageTypes.NODE_LIST, this.nodeList);
+                this.sendMessage(dstNode, listMsg);
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void mergeMembershipLists(DynamoNode srcNode, Object payload) {
+        if (payload instanceof ArrayList<?>) {
+            ArrayList<DynamoNode> remoteNodesList = (ArrayList<DynamoNode>) payload;
+            /* add srcNode to list too, since it has sent, so should be alive ! */
+            remoteNodesList.add(srcNode);
+            synchronized (DynamoServer.this.deadList) {
+                synchronized (DynamoServer.this.nodeList) {
+                     /* Do the same with rest of the nodes */
+                    for (DynamoNode remoteNode : remoteNodesList) {
+                        if (DynamoServer.this.nodeList.contains(remoteNode)) {
+                                /* Just update the heartbeat to the latest one
+                                 * and reset timer */
+                                DynamoNode localNode = DynamoServer.this.nodeList.get(DynamoServer.this.nodeList.indexOf((remoteNode)));
+                                if (remoteNode.getHeartbeat() > localNode.getHeartbeat()) {
+                                    localNode.setHeartbeat(remoteNode.getHeartbeat());
+                                    localNode.resetTimer();
+                                }
+                        } else {
+                                /* local list does not contain remoteNode */
+
+                            if (DynamoServer.this.deadList.contains(remoteNode)) {
+                                /* The remoteNode previously was there in the local list
+                                 * but timed out. So revive it!!
+                                 * Updated heartbeat to the one received latest, and start timer*/
+                                DynamoNode localDeadNode = DynamoServer.this.deadList.get(DynamoServer.this.deadList.indexOf(remoteNode));
+                                if (remoteNode.getHeartbeat() > localDeadNode.getHeartbeat()) {
+                                    DynamoServer.this.deadList.remove(localDeadNode);
+                                    DynamoNode newNode =
+                                            new DynamoNode(remoteNode.name, remoteNode.getAddress(), this, remoteNode.getHeartbeat(), this.ttl);
+                                    DynamoServer.this.nodeList.add(newNode);
+                                    newNode.startTimer();
+                                }
+
+                            } else {
+                                /* Probably a new member, add it to the list, use remote heartbeat,
+                                 * start timer */
+                                DynamoNode newNode =
+                                        new DynamoNode(remoteNode.name, remoteNode.getAddress(), this, remoteNode.getHeartbeat(), this.ttl);
+                                DynamoServer.this.nodeList.add(newNode);
+                                newNode.startTimer();
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("[WARN] List received is not instanceof ArrayList");
+        }
+
+    }
+
+//    private ArrayList<DynamoNode> cloneArrayList(ArrayList<DynamoNode> nodeList) {
+//        ArrayList<DynamoNode> newList = new ArrayList<>();
+//        for(DynamoNode node : nodeList) {
+//            newList.add((DynamoNode)node.clone());
+//        }
+//        return newList;
+//    }
+
     private void start() throws InterruptedException {
         ExecutorService exec = Executors.newCachedThreadPool();
         exec.execute(new Receiver());
-        exec.execute(new PingSender());
+        //exec.execute(new PingSender());
+        exec.execute(new Gossiper());
 
         while (true) {
             TimeUnit.SECONDS.sleep(10);
@@ -148,7 +280,8 @@ public class DynamoServer {
         } else {
             ArrayList<String> addr_list =
                     new ArrayList<>(Arrays.asList(args[2].split(",")));
-            DynamoServer server = new DynamoServer(args[0], args[1], addr_list);
+            DynamoServer server =
+                    new DynamoServer(args[0], args[1], addr_list, Integer.parseInt(args[3]), Integer.parseInt(args[4]));
             server.start();
         }
     }
